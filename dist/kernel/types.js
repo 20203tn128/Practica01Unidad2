07088ce5416bfa95f19d9da9efc8eda854ae4872
LizClaudia <20203tn128@utez.edu.mx>
2023-10-10 10:08:21 -0600

    Modificaciones

---
+++ dist/kernel/types.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Error = void 0;
+var Error;
+(function (Error) {
+    Error["MISSING_FILEDS"] = "MissingFields";
+    Error["RECORD_NOT_REGISTERED"] = "RecordNoRegistered";
+    Error["RECORD_NOT_UPDATED"] = "RecordNotUpdated";
+    Error["NO_DATA_FOUND"] = "NoDataFound";
+    Error["CREDENTIALS_MISMATCH"] = "CredentialsMismatch";
+    Error["UNAUTHORIZED"] = "Unauthorized";
+})(Error || (exports.Error = Error = {}));
+//# sourceMappingURL=types.js.map
--- src/config/express.ts
+++ src/config/express.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from 'path';
 import { userRouter } from "../modules/user/adapters/user.controller";
 const cors = require('cors');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname,'../public')));
+
 app.use(
     cors({  // cors se utiliza para que se estblezca en la app que dominios pueden hacer uso de la app
         origin:'*'
